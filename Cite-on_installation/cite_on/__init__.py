075cbd46144a4dcca0c76430455ed984ed0dbe92
doug <dsnd@umd.edu>
2022-07-29 16:08:11 -0400

    deleting citeon to ignore

--- Cite-on_installation/cite_on/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Detector import Detector
-from .AsyncDetector import AsyncDetector
-#from .Tracker import Tracker
-from .Extractor import Extractor
-from . import utils
-
-__all__=['Detector','AsyncDetector','Extractor','utils']
