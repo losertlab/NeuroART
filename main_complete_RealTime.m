dece16a62639576620909e9d5e5c54517e573bab
dougscalioni <dsnd@umd.edu>
2022-09-12 15:54:29 -0400

    fixed new citeon

--- main_complete_RealTime.m
+++ main_complete_RealTime.m
@@ -148,8 +148,8 @@ elseif inputParams.CFIND == 3 % Cite-on
 %%     system('activate cite-on & python test.py')
     ups = 2.0;
     th = 0.5;
-    [~,b] = system(['activate cite-on & python test.py' ' -x ' num2str(exptVars.dimX) ' -y ' num2str(exptVars.dimY) ' -n ' num2str(length(batchSettings.frameBlock)) ' -p ' inputParams.imageFile ' -u ' num2str(ups) ' -t ' num2str(th)]);
-    %disp(b);
+    [~,b] = system(['activate citeon & python test.py' ' -x ' num2str(exptVars.dimX) ' -y ' num2str(exptVars.dimY) ' -n ' num2str(length(batchSettings.frameBlock)) ' -p "' inputParams.imageFile '" -u ' num2str(ups) ' -t ' num2str(th)]);
+    disp(b);
     T = readtable('cell_coordinates.csv');
     
     cell_centroids(:,1) = T.Var2; %yc
--- test.py
+++ test.py
@@ -104,7 +104,7 @@ median=np.median(img,axis=2)
 #ups = 2.6 # to make init_upscaling = upscaling
 #th = 0.5
 
-detector=Detector('citeon/wheighs/', gpu='0', init_shape=img.shape[:2], init_upscaling=ups)
+detector=Detector('citeon/weights/', gpu='0', init_shape=img.shape[:2], init_upscaling=ups)
 partial=time()
 boxes=detector.detect(median,threshold=th,upscaling=ups)
 #print('No of cells: ' + str(boxes.shape[0]));
